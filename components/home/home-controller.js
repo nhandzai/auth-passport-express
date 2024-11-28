25538fbf31733842a3abaf76fc801708d9c85efb
PhamHoangKha1403 <phamhoangkha14032004@gmail.com>
2024-11-28 17:08:30 +0700

    fix database

--- components/home/home-controller.js
+++ components/home/home-controller.js
@@ -4,7 +4,7 @@ const { renderHomePage } = require('./home-view');
 
 async function getHome(req, res, next) {
   try {
-    console.log('Fetching products...',getSizesByProductId(1));
+   
     const products = await sortProductsByPrice(4);
     renderHomePage(res, products);
   } catch (error) {
